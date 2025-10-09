94c1a0e4bb7d146975abe41ae70ea6074a0356dd
wardenein <96897527+wardenein@users.noreply.github.com>
2025-10-09 15:47:58 -0400

    Create info.js

---
+++ frontend/pages/info.js
@@ -0,0 +1,8 @@
+export default function Info() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold text-primary">Info Page</h1>
+      <p className="mt-4 text-lg text-gray-600">Navbar routing works!</p>
+    </div>
+  );
+}
