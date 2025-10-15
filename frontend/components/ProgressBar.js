6bc53064a9b392862bd54130948f67db9fc29c36
wardenein <96897527+wardenein@users.noreply.github.com>
2025-10-15 15:34:03 -0400

    Update ProgressBar.js

--- frontend/components/ProgressBar.js
+++ frontend/components/ProgressBar.js
@@ -1,1 +1,10 @@
-
+export default function ProgressBar({ progress }) {
+  return (
+    <div className="w-full bg-gray-800 rounded-sm h-4">
+      <div
+        className="bg-white h-4 rounded-sm"
+        style={{ width: `${progress * 100}%` }}
+      ></div>
+    </div>
+  );
+}
